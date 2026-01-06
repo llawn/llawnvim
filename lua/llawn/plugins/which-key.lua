6572e4e20cda8f58b6f8de788ec5c2dd86a52ea4
llawn <llawn06@gmail.com>
2026-01-06 16:54:37 +0100

    style: style for which-key

--- lua/llawn/plugins/which-key.lua
+++ lua/llawn/plugins/which-key.lua
@@ -1,8 +1,22 @@
+--- @brief Plugin configuration for which-key
+--- which-key displays available keybindings in a popup menu
+---
+
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
   },
   keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
   },
 }
