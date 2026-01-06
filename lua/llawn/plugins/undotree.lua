75b0dabba6780c621617fbc498175defc53f0e29
llawn <llawn06@gmail.com>
2026-01-06 16:55:06 +0100

    docs: doc for undotree

--- lua/llawn/plugins/undotree.lua
+++ lua/llawn/plugins/undotree.lua
@@ -1,8 +1,18 @@
-return {
-    "mbbill/undotree",
+--- @brief Plugin configuration for undotree
+--- undotree visualizes the undo history as a tree for easy navigation
+---
 
-    config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle UndoTree"})
-    end
-  }
+return {
+  "mbbill/undotree",
+
+  config = function()
+    -- Keybinding to toggle the undotree panel
+    vim.keymap.set(
+      "n",
+      "<leader>u",
+      vim.cmd.UndotreeToggle,
+      { desc = "Toggle UndoTree"}
+    )
+  end
+}
 
