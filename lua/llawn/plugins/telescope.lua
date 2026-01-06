3ca5bf4543a4c2267645db60539bfe298ed4a0bc
llawn <llawn06@gmail.com>
2026-01-06 16:55:17 +0100

    docs: docs for telescope

--- lua/llawn/plugins/telescope.lua
+++ lua/llawn/plugins/telescope.lua
@@ -1,25 +1,34 @@
+--- @brief Plugin configuration for telescope
+--- telescope is a highly extendable fuzzy finder for Neovim
+--- Provides powerful search and navigation capabilities
+---
+
 return {
-    'nvim-telescope/telescope.nvim', 
-      dependencies = { 'nvim-lua/plenary.nvim' },
-     config = function()
-        require('telescope').setup({})
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' }, -- Required utility functions
+  config = function()
+    -- Basic telescope setup with default configuration
+    require('telescope').setup({})
 
-        local builtin = require('telescope.builtin')
+    -- Load builtin functions
+    local builtin = require('telescope.builtin')
 
-        vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>tg', builtin.git_files, { desc = 'Telescope git files' })
-        vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
+    -- Telescope keybindings for various search functions
 
-    	-- Search the current word under the cursor
-        vim.keymap.set(
-            'n',
-            '<leader>tw',
-            function()
-                local word = vim.fn.expand("<cword>")
-                builtin.grep_string({ search = word })
-            end,
-            { desc = 'Telescope find word' }
-        )
-    end
+    vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>tg', builtin.git_files, { desc = 'Telescope git files' })
+    vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>th', builtin.help_tags, { desc = 'Telescope help tags' })
+
+    -- Search the current word under the cursor
+    vim.keymap.set(
+      'n',
+      '<leader>tw',
+      function()
+        local word = vim.fn.expand("<cword>")
+        builtin.grep_string({ search = word })
+      end,
+      { desc = 'Telescope find word' }
+    )
+  end
 }
