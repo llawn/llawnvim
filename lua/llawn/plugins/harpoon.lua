991f03244ca6a895688dfb9df63941296d1b9680
llawn <llawn06@gmail.com>
2026-01-06 16:52:31 +0100

    style: fix style for harpoon

--- lua/llawn/plugins/harpoon.lua
+++ lua/llawn/plugins/harpoon.lua
@@ -1,32 +1,49 @@
 --- @brief Harpoon plugin configuration for quick file navigation in Neovim
 --- Provides keybindings for adding and selecting frequently accessed files
+---
 
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
 
-    config = function()
-        local harpoon = require("harpoon")
+  config = function()
+    local harpoon = require("harpoon")
 
-        harpoon:setup()
+    harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, {
-            desc = "Add file to harpoon list"
-        })
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {
-            desc = "Toggle harpoon quick menu"
-        })
+    vim.keymap.set(
+      "n",
+      "<leader>a",
+      function() harpoon:list():add() end,
+      { desc = "Add file to harpoon list"}
+    )
+    vim.keymap.set(
+      "n",
+      "<C-e>",
+      function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+      { desc = "Toggle harpoon quick menu" }
+    )
 
-        vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end, {
-            desc = "Select harpoon file 1"
-        })
-        vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end, {
-            desc = "Select harpoon file 2"
-        })
-        vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end, {
-            desc = "Select harpoon file 3"
-        })
+    vim.keymap.set(
+      "n",
+      "<C-1>",
+      function() harpoon:list():select(1) end,
+      { desc = "Select harpoon file 1" }
+    )
 
-    end
+    vim.keymap.set(
+      "n",
+      "<C-2>",
+      function() harpoon:list():select(2) end,
+      { desc = "Select harpoon file 2" }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<C-3>",
+      function() harpoon:list():select(3) end,
+      { desc = "Select harpoon file 3" }
+    )
+  end
 }
