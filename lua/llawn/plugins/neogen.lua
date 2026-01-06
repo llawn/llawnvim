ad67ae841f50f84c8af628159645d1f687e65838
llawn <llawn06@gmail.com>
2026-01-06 16:53:52 +0100

    style: fix style for neogen

--- lua/llawn/plugins/neogen.lua
+++ lua/llawn/plugins/neogen.lua
@@ -1,29 +1,38 @@
+--- @brief Plugin configuration for neogen
+--- neogen is a Neovim plugin that generates docstrings (annotations) for functions and types
+--- It uses Treesitter for parsing and LuaSnip for snippet insertion
+--- 
+
 return {
-    "danymat/neogen",
-    description = "Generate annotations (docstrings) for functions and types using Treesitter and LuaSnip",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "L3MON4D3/LuaSnip",
-    },
-    config = function()
-        local neogen = require("neogen")
+  "danymat/neogen",
+  description = "Generate annotations (docstrings) for functions and types using Treesitter and LuaSnip",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter", -- Required for syntax parsing
+    "L3MON4D3/LuaSnip", -- Required for snippet engine
+  },
+  config = function()
+    -- Setup neogen with LuaSnip as the snippet engine
+    local neogen = require("neogen")
 
-        neogen.setup({
-            snippet_engine = "luasnip"
-        })
+    neogen.setup({
+      snippet_engine = "luasnip"
+    })
 
-        -- Generate function docstring
-        vim.keymap.set("n", "<leader>nf", function()
-            neogen.generate({ type = "func" })
-        end, { desc = "Generate function docstring" })
+    -- Generate function docstring
+    vim.keymap.set(
+      "n",
+      "<leader>nf",
+      function() neogen.generate({ type = "func" }) end,
+      { desc = "Generate function docstring" }
+    )
 
-        -- Generate type/class docstring
-        vim.keymap.set("n", "<leader>nt", function()
-            neogen.generate({ type = "type" })
-        end, { desc = "Generate type/class docstring" })
-
-    end,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*"
+    -- Generate type/class docstring
+    vim.keymap.set(
+      "n",
+      "<leader>nt",
+      function() neogen.generate({ type = "type" }) end,
+      { desc = "Generate type/class docstring" }
+    )
+  end,
 }
 
