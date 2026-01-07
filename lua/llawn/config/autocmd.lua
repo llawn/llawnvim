942b04e1481bbcc4fb963b663f46bb454eab82af
llawn <llawn06@gmail.com>
2026-01-07 07:36:02 +0100

    feat: undo files in an undodir directory
    - undo files are now in an ~/.undodir
    - add a :CleanUndo command to clean the directory

--- lua/llawn/config/autocmd.lua
+++ lua/llawn/config/autocmd.lua
@@ -17,3 +17,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- ============================================================================
+-- Clean Undo Files Command
+-- ============================================================================
+
+vim.api.nvim_create_user_command("CleanUndo", function()
+  local undodir = vim.fn.expand("~/.undodir", 1)
+  if vim.fn.isdirectory(undodir) == 1 then
+    vim.fn.system("rm -rf " .. undodir .. "/*")
+    vim.notify("Undo files cleaned.", vim.log.levels.INFO)
+  else
+    vim.notify("Undodir does not exist.", vim.log.levels.WARN)
+  end
+end, { desc = "Clean all persistent undo files" })
