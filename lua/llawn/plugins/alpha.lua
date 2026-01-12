5e17ee981d9bda3ef66bad5f34c2957fb8220b27
llawn <llawn06@gmail.com>
2026-01-12 16:42:32 +0100

    feat: add alpha to the plugins

--- lua/llawn/plugins/alpha.lua
+++ lua/llawn/plugins/alpha.lua
@@ -19,7 +19,7 @@ return {
       dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("f", "󰈞  Find file", ":Telescope find_files<CR>"),
       dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
-       dashboard.button("s", "  Settings", ":e ~/.config/nvim<CR>"),
+      dashboard.button("s", "  Settings", ":e ~/.config/nvim<CR>"),
       dashboard.button("q", "󰈆  Quit", ":qa<CR>"),
     }
 
