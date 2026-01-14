96a2d9e2aa4bcaa5bcfc5f7c758f027cb570bcba
llawn <llawn06@gmail.com>
2026-01-15 00:48:25 +0100

    fix: fix generate changelog script

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -23,91 +23,83 @@ CHANGELOG_BODY=""
 DOCS_BODY=""
 
 generate_entry() {
-    local range="$1"
-    local version="$2"
-    local date="$3"
-    local raw_commits=$(git log --pretty=format:"%s" $range)
-    local added=""
-    local changed=""
-    local deprecated=""
-    local removed=""
-    local fixed=""
-    local security=""
-    local docs=""
-    local style=""
-    local refactor=""
-    local test=""
-    local chore=""
-    local feat=""
-    local other=""
+  local range="$1"
+  local version="$2"
+  local date="$3"
+  local raw_commits=""
+  local feat=""
+  local fixed=""
+  local docs=""
+  local style=""
+  local refactor=""
+  local test=""
+  local chore=""
+  local other=""
 
-    while IFS= read -r commit; do
-        case "$commit" in
-            feat:*)
-                feat="$feat\n- ${commit#feat: }"
-                ;;
-            fix:*)
-                fixed="$fixed\n- ${commit#fix: }"
-                ;;
-            docs:*)
-                docs="$docs\n- ${commit#docs: }"
-                ;;
-            style:*)
-                style="$style\n- ${commit#style: }"
-                ;;
-            refactor:*)
-                refactor="$refactor\n- ${commit#refactor: }"
-                ;;
-            test:*)
-                test="$test\n- ${commit#test: }"
-                ;;
-            chore:*)
-                chore="$chore\n- ${commit#chore: }"
-                ;;
-            *)
-                other="$other\n- $commit"
-                ;;
-        esac
-    done <<< "$raw_commits"
+  raw_commits=$(git log --pretty=format:"%s" "$range")
 
-    local entry="## [$version] - $date\n\n"
-    [ -n "$feat" ] && entry="$entry### Added\n$feat\n\n"
-    [ -n "$changed" ] && entry="$entry### Changed\n$changed\n\n"
-    [ -n "$deprecated" ] && entry="$entry### Deprecated\n$deprecated\n\n"
-    [ -n "$removed" ] && entry="$entry### Removed\n$removed\n\n"
-    [ -n "$fixed" ] && entry="$entry### Fixed\n$fixed\n\n"
-    [ -n "$security" ] && entry="$entry### Security\n$security\n\n"
-    [ -n "$docs" ] && entry="$entry### Documentation\n$docs\n\n"
-    [ -n "$style" ] && entry="$entry### Style\n$style\n\n"
-    [ -n "$refactor" ] && entry="$entry### Refactored\n$refactor\n\n"
-    [ -n "$test" ] && entry="$entry### Tests\n$test\n\n"
-    [ -n "$chore" ] && entry="$entry### Chore\n$chore\n\n"
-    [ -n "$other" ] && entry="$entry$other\n\n"
+  while IFS= read -r commit; do
+    case "$commit" in
+      feat:*)
+        feat="$feat\n- ${commit#feat: }"
+        ;;
+      fix:*)
+        fixed="$fixed\n- ${commit#fix: }"
+        ;;
+      docs:*)
+        docs="$docs\n- ${commit#docs: }"
+        ;;
+      style:*)
+        style="$style\n- ${commit#style: }"
+        ;;
+      refactor:*)
+        refactor="$refactor\n- ${commit#refactor: }"
+        ;;
+      test:*)
+        test="$test\n- ${commit#test: }"
+        ;;
+      chore:*)
+        chore="$chore\n- ${commit#chore: }"
+        ;;
+      *)
+        other="$other\n- $commit"
+        ;;
+    esac
+  done <<< "$raw_commits"
 
-    printf "%s" "$entry"
+  local entry="## [$version] - $date\n\n"
+  [ -n "$feat" ] && entry="$entry### Added\n$feat\n\n"
+  [ -n "$fixed" ] && entry="$entry### Fixed\n$fixed\n\n"
+  [ -n "$docs" ] && entry="$entry### Documentation\n$docs\n\n"
+  [ -n "$style" ] && entry="$entry### Style\n$style\n\n"
+  [ -n "$refactor" ] && entry="$entry### Refactored\n$refactor\n\n"
+  [ -n "$test" ] && entry="$entry### Tests\n$test\n\n"
+  [ -n "$chore" ] && entry="$entry### Chore\n$chore\n\n"
+  [ -n "$other" ] && entry="$entry$other\n\n"
+
+  printf "%s" "$entry"
 }
 
 for TAG in $TAGS; do
-    DATE=$(git log -1 --format=%ai ${TAG} | cut -d' ' -f1)
-    ENTRY=$(generate_entry "${PREV_COMMIT}..${TAG}" "$TAG" "$DATE")
-    CHANGELOG_BODY="$ENTRY\n\n$CHANGELOG_BODY"
-
-    # Detailed for docs
-    DETAILED_COMMITS=$(git log --stat --pretty=format:"commit %h %s%nAuthor: %an <%ae>%nDate: %ad%n%n%b" ${PREV_COMMIT}..${TAG} | sed 's/^/    /')
-    DOCS_ENTRY="## [$TAG] - $DATE\n\n### Details of Changes\n\n\`\`\`\n$DETAILED_COMMITS\n\`\`\`\n\n"
-    DOCS_BODY="$DOCS_ENTRY\n\n$DOCS_BODY"
-    PREV_COMMIT=${TAG}
-done
+  DATE=$(git log -1 --format=%ai "${TAG}" | cut -d' ' -f1)
+  ENTRY=$(generate_entry "${PREV_COMMIT}..${TAG}" "$TAG" "$DATE")
+  CHANGELOG_BODY="$ENTRY$CHANGELOG_BODY"
+  # Detailed for docs
+  DETAILED_COMMITS=$(git log --stat --pretty=format:"commit %h %s%nAuthor: %an <%ae>%nDate: %ad%n%n%b" "${PREV_COMMIT}".."${TAG}" | sed 's/^/ /')
+  DOCS_ENTRY="## [$TAG] - $DATE\n\n### Details of Changes\n\n\`\`\`\n$DETAILED_COMMITS\n\`\`\`\n\n"
+  DOCS_BODY="$DOCS_ENTRY$DOCS_BODY"
+  PREV_COMMIT=${TAG}
+  done
 
 # Commits after the last tag
 DATE=$(date +%Y-%m-%d)
 NEXT_TAG="Unreleased"
 ENTRY=$(generate_entry "${PREV_COMMIT}..HEAD" "$NEXT_TAG" "$DATE")
-CHANGELOG_BODY="$ENTRY\n\n$CHANGELOG_BODY"
+CHANGELOG_BODY="$ENTRY$CHANGELOG_BODY"
 
-DETAILED_COMMITS=$(git log --stat --pretty=format:"commit %h %s%nAuthor: %an <%ae>%nDate: %ad%n%n%b" ${PREV_COMMIT}..HEAD | sed 's/^/    /')
+DETAILED_COMMITS=$(git log --stat --pretty=format:"commit %h %s%nAuthor: %an <%ae>%nDate: %ad%n%n%b" "${PREV_COMMIT}"..HEAD | sed 's/^/  /')
 DOCS_ENTRY="## [$NEXT_TAG] - $DATE\n\n### Details of Changes\n\n\`\`\`\n$DETAILED_COMMITS\n\`\`\`\n\n"
-DOCS_BODY="$DOCS_ENTRY\n\n$DOCS_BODY"
+DOCS_BODY="$DOCS_ENTRY$DOCS_BODY"
 
 # Write CHANGELOG.md
 echo -e "$CHANGELOG_HEADER\n\n$CHANGELOG_BODY" > CHANGELOG.md
