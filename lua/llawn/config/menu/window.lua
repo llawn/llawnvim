5312bb1105254022cc19cb9c576859f2ca8722ac
llawn <llawn06@gmail.com>
2026-01-17 23:48:07 +0100

    style: format window.lua

--- lua/llawn/config/menu/window.lua
+++ lua/llawn/config/menu/window.lua
@@ -1,28 +1,30 @@
---- @brief Window menu
+--- Window menu
 
 local M = {}
 
 M.window = {}
 
+--- Window menu
+--- @return nil
 M.window.menu = function()
   local choices = {
     { "Horizontal Split", function() vim.cmd("split") end },
-    { "Vertical Split", function() vim.cmd("vsplit") end },
-    { "Move Left", function() vim.cmd("wincmd h") end },
-    { "Move Right", function() vim.cmd("wincmd l") end },
-    { "Move Up", function() vim.cmd("wincmd k") end },
-    { "Move Down", function() vim.cmd("wincmd j") end },
-    { "Close Window", function() vim.cmd("confirm close") end },
+    { "Vertical Split",   function() vim.cmd("vsplit") end },
+    { "Move Left",        function() vim.cmd("wincmd h") end },
+    { "Move Right",       function() vim.cmd("wincmd l") end },
+    { "Move Up",          function() vim.cmd("wincmd k") end },
+    { "Move Down",        function() vim.cmd("wincmd j") end },
+    { "Close Window",     function() vim.cmd("confirm close") end },
   }
 
-  vim.ui.select(choices, {
-    prompt = "Window Menu:",
-    format_item = function(item) return item[1] end
-  }, function(choice)
-      if choice then
-        choice[2]()
-      end
-    end)
+  vim.ui.select(
+    choices,
+    {
+      prompt = "Window Menu:",
+      format_item = function(item) return item[1] end
+    },
+    function(choice) if choice then choice[2]() end end
+  )
 end
 
 return M
