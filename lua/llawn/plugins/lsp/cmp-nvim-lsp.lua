4e65bbd9fe68c8a71f937e78cbe4eb70bd7403fd
llawn <llawn06@gmail.com>
2026-01-19 21:43:29 +0100

    style: style for lsp cmp plugin

--- lua/llawn/plugins/lsp/cmp-nvim-lsp.lua
+++ lua/llawn/plugins/lsp/cmp-nvim-lsp.lua
@@ -1,28 +1,23 @@
---- @brief
---- This file configures the cmp-nvim-lsp plugin for Neovim, which enables
---- autocompletion capabilities for Language Server Protocol (LSP) servers.
---- It integrates with cmp (completion) plugin to provide intelligent
---- code completion suggestions.
----
---- Dependencies:
----   - antosha417/nvim-lsp-file-operations: Handles LSP file operations
----   - folke/lazydev.nvim: Lazy loading for development plugins
----
+-- Plugin: cmp-nvim-lsp
+-- Description: Autocompletion capabilities for LSP servers.
+--              It integrates with cmp plugin to provide intelligent
+--              code completion suggestions.
+-- Dependencies:
+--   - antosha417/nvim-lsp-file-operations: Handles LSP file operations
+--   - folke/lazydev.nvim: Lazy loading for development plugins
 
 return {
   "hrsh7th/cmp-nvim-lsp",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     { "antosha417/nvim-lsp-file-operations", config = true },
-    { "folke/lazydev.nvim", opts = {} },
+    { "folke/lazydev.nvim",                  opts = {} },
   },
   config = function()
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Apply capabilities to all LSP configurations
-    vim.lsp.config("*", {
-      capabilities = capabilities,
-    })
+    vim.lsp.config("*", { capabilities = capabilities })
   end,
 }
