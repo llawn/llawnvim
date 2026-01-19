f0ad3b0c9a14b8043a074d7a06c4cae617e4a4f7
llawn <llawn06@gmail.com>
2026-01-19 14:11:43 +0100

    style: neogen
    change also keymap

--- lua/llawn/plugins/neogen.lua
+++ lua/llawn/plugins/neogen.lua
@@ -1,14 +1,12 @@
---- @brief Plugin configuration for neogen
---- neogen is a Neovim plugin that generates docstrings (annotations) for functions and types
---- It uses Treesitter for parsing and LuaSnip for snippet insertion
---- 
+-- Plugin: Neogen
+-- Description: Generates docstrings for functions and types with Treesitter and LuaSnip integration
 
 return {
   "danymat/neogen",
   description = "Generate annotations (docstrings) for functions and types using Treesitter and LuaSnip",
   dependencies = {
     "nvim-treesitter/nvim-treesitter", -- Required for syntax parsing
-    "L3MON4D3/LuaSnip", -- Required for snippet engine
+    "L3MON4D3/LuaSnip",                -- Required for snippet engine
   },
   config = function()
     -- Setup neogen with LuaSnip as the snippet engine
@@ -21,18 +19,17 @@ return {
     -- Generate function docstring
     vim.keymap.set(
       "n",
-      "<leader>nf",
+      "<leader>pd",
       function() neogen.generate({ type = "func" }) end,
-      { desc = "Generate function docstring" }
+      { desc = "Function Doc" }
     )
 
     -- Generate type/class docstring
     vim.keymap.set(
       "n",
-      "<leader>nt",
+      "<leader>pt",
       function() neogen.generate({ type = "type" }) end,
-      { desc = "Generate type/class docstring" }
+      { desc = "Type Doc" }
     )
   end,
 }
-
