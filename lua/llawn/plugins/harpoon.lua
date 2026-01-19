11938e5bdd562aff0e8fb421024bcf9d6c2797c1
llawn <llawn06@gmail.com>
2026-01-19 12:24:31 +0100

    feat: telescope menu for harpoon with deletion action
    - add telescope menu
    - use D to delete on the menu
    - add keymap for for previous/next harpoon

--- lua/llawn/plugins/harpoon.lua
+++ lua/llawn/plugins/harpoon.lua
@@ -1,6 +1,7 @@
---- @brief Harpoon plugin configuration for quick file navigation in Neovim
---- Provides keybindings for adding and selecting frequently accessed files
----
+-- Plugin: Harpoon
+-- Description: Plugin configuration for quick file navigation in Neovim
+--              Provides keybindings for adding and selecting frequently accessed files
+-- Note: See harpoon files in ~/.local/share/nvim/harpoon/
 
 return {
   "ThePrimeagen/harpoon",
@@ -10,40 +11,119 @@ return {
   config = function()
     local harpoon = require("harpoon")
 
-    harpoon:setup()
+    -- basic telescope configuration
+    local conf = require("telescope.config").values
+
+
+    --- Deletes a harpoon item from the list
+    --- @param selection table The selected entry to delete
+    local function delete_harpoon_item(selection)
+      local list = harpoon:list()
+      -- find the index of the selected item
+      local index
+      for i, item in ipairs(list.items) do
+        if item and item.value == selection.value.value then
+          index = i
+          break
+        end
+      end
+      if index then
+        table.remove(list.items, index)
+        list._length = list._length - 1
+      end
+    end
+
+    --- Opens a telescope picker for harpoon files
+    --- @param harpoon_files table The harpoon list
+    local function toggle_telescope(harpoon_files)
+      local finder = require("telescope.finders").new_table({
+        results = harpoon_files.items,
+        entry_maker = function(entry)
+          return {
+            value = entry,
+            path = entry.value,
+            display = entry.value,
+            ordinal = entry.value,
+          }
+        end
+      })
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon | [D]elete",
+        finder = finder,
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          local actions = require("telescope.actions")
+          local action_state = require("telescope.actions.state")
+
+          map("i", "D", function()
+            local selection = action_state.get_selected_entry()
+            delete_harpoon_item(selection)
+            actions.close(prompt_bufnr)
+            vim.schedule(function () toggle_telescope(harpoon:list()) end)
+          end)
+
+          map("n", "D", function()
+            local selection = action_state.get_selected_entry()
+            delete_harpoon_item(selection)
+            actions.close(prompt_bufnr)
+            vim.schedule(function () toggle_telescope(harpoon:list()) end)
+          end)
+
+          return true
+        end,
+      }):find()
+    end
 
     vim.keymap.set(
       "n",
       "<leader>a",
       function() harpoon:list():add() end,
-      { desc = "Add file to harpoon list"}
+      { desc = "Add to Harpoon" }
     )
+
     vim.keymap.set(
       "n",
       "<C-e>",
-      function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-      { desc = "Toggle harpoon quick menu" }
+      function () toggle_telescope(harpoon:list()) end,
+      { desc = "Open harpoon window" }
     )
 
     vim.keymap.set(
       "n",
       "<C-1>",
       function() harpoon:list():select(1) end,
-      { desc = "Select harpoon file 1" }
+      { desc = "Harpoon 1" }
     )
 
     vim.keymap.set(
       "n",
       "<C-2>",
       function() harpoon:list():select(2) end,
-      { desc = "Select harpoon file 2" }
+      { desc = "Harpoon 2" }
     )
 
     vim.keymap.set(
       "n",
       "<C-3>",
       function() harpoon:list():select(3) end,
-      { desc = "Select harpoon file 3" }
+      { desc = "Harpoon 3" }
+    )
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set(
+      "n",
+      "<leader>hp",
+      function() harpoon:list():prev({ ui_nav_wrap = true }) end,
+      { desc = "Harpoon previous" }
+    )
+
+    vim.keymap.set(
+      "n",
+      "<leader>hn",
+      function() harpoon:list():next({ ui_nav_wrap = true }) end,
+      { desc = "Harpoon next" }
     )
   end
 }
